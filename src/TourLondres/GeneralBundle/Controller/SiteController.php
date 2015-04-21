d516734450eabec7ad7733285181b033cc477fa8
Jorge Agundez Medina <jamedina97@gmail.com>
2015-04-21 01:11:07 +0100

    home adding, languages and new routing

--- src/TourLondres/GeneralBundle/Controller/SiteController.php
+++ src/TourLondres/GeneralBundle/Controller/SiteController.php
@@ -6,7 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class SiteController extends Controller
 {
    
+    public function staticAction($page, $_locale)
+    {
+        return $this->render('GeneralBundle:Site:'. $_locale . '_' . $page .'.html.twig', array ('template' => $page));
+    }
+
 }
--- web/js/control.js
+++ web/js/control.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
     
-    // Index Slider
-    var i = 1; 
-    $('#home img:nth-child(1)').fadeIn(0);
-    $.timer(4000, function(temporizador){
-        i++;
-            if ( i < 10 ){
-                $('#home img:nth-child('+ (i-1) +')').fadeOut(1000);
-                $('#home img:nth-child('+ i +')').fadeIn(500);
-            }else{ 
-                $('#home img:nth-child('+ (i-1) +')').fadeOut(500);
-                $('#home img:nth-child(1)').fadeIn(1000);
-                i=1;
-            };  
-    });
+    $('#carousel-example-generic').carousel();
+
 });
 
