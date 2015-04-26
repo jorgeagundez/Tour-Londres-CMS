6524862ba1a8b828e7f546d30de381c862c223c2
Jorge Agundez Medina <jamedina97@gmail.com>
2015-04-26 22:36:59 +0100

    tour dashboard creado

--- src/TourLondres/DashboardBundle/Controller/AdmintoursController.php
+++ src/TourLondres/DashboardBundle/Controller/AdmintoursController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use TourLondres\DashboardBundle\Entity\Tour;
 
 class AdmintoursController extends Controller
 {
@@ -23,13 +24,30 @@ class AdmintoursController extends Controller
 
     /**
      * @Route("/admin/tour/add")
-     * @Template()
+     * @Template("TourLondresDashboardBundle:Admintours:edit.html.twig")
      */
-    public function addAction()
+    public function addAction(Request $request )
     {
-        return array(
-                // ...
-            );    }
+        $tour = new Tour();
+        $form = $this->createFormBuilder($tour)
+            ->add('esname', 'text', array( 'label' => 'Nombre del Tour' ))
+            ->add('esdescription', 'text', array( 'label' => 'Breve descripcion' ))
+            ->add('esbody', 'textarea', array( 'label' => 'Texto completo' ))
+            ->add('esday', 'text', array( 'label' => 'Dia de la semana' ))
+            ->add('esplace', 'text', array( 'label' => 'Lugar de encuentro' ))
+            ->add('enname', 'text', array( 'label' => 'Nombre del Tour' ))
+            ->add('endescription', 'text', array( 'label' => 'Breve descripcion' ))
+            ->add('enbody', 'textarea', array( 'label' => 'Texto completo' ))
+            ->add('esday', 'text', array( 'label' => 'Dia de la semana' ))
+            ->add('esplace', 'text', array( 'label' => 'Lugar de encuentro' ))
+            ->add('type', 'choice', array('choices' => array( 'privado' => 'Privado', 'semanal' => 'Semanal'), 'label' => 'Tipo de Tour' ))
+            ->add('time', 'text', array( 'label' => 'Hora' ))
+            ->add('submit', 'button')
+            ->getForm();
+
+        // $form->handleRequest($request);
+        return array('form' => $form->createView());
+    }
 
     /**
      * @Route("/admin/tour/edit/{id}")
